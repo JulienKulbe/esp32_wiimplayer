fd2f1b5788b5b5b45e9ab41432ffb341dc66a627
Julien Kulbe <julien.kulbe@googlemail.com>
2023-06-17 13:52:46 +0200

    try to integrate graphics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
+use display_interface_spi::SPIInterfaceNoCS;
+use embedded_graphics::{draw_target::DrawTarget, pixelcolor::Rgb565, prelude::*};
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use log::*;
+use mipidsi::Builder;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    info!("Hello, world!");
+    let spi = todo!();
+    let dc = todo!();
+    let delay = todo!();
+    let rst = todo!();
+
+    // create a DisplayInterface from SPI and DC pin, with no manual CS control
+    let di = SPIInterfaceNoCS::new(spi, dc);
+    // create the ILI9486 display driver from the display interface and optional RST pin
+    let mut display = Builder::st7789(di)
+        .init(&mut delay, Some(rst))
+        .map_err(|_| Box::<dyn Error>::from("display init"))?;
+
+    // clear the display to black
+    display
+        .clear(Rgb565::RED)
+        .map_err(|_| Box::<dyn Error>::from("clear display"))?;
+
+    Ok(())
 }
