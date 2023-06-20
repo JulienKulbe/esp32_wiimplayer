61d4f82abfc40f2e51dffcdbadac561a238f4ae9
Julien Kulbe <julien.kulbe@googlemail.com>
2023-06-20 22:03:03 +0200

    fixed colors

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-
-use std::thread;
-use std::time::Duration;
-
+use anyhow::anyhow;
 use anyhow::Result;
-
+use display_interface_parallel_gpio::*;
+use embedded_graphics::mono_font::MonoTextStyle;
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::*;
+use embedded_graphics::text::Text;
+use embedded_graphics::{
+    mono_font::ascii::FONT_6X10,
+    primitives::{PrimitiveStyleBuilder, Rectangle},
+};
 use esp_idf_hal::delay::Ets;
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
-
-use display_interface_parallel_gpio::*;
-
-use embedded_graphics::pixelcolor::Rgb565;
-use embedded_graphics::prelude::*;
-
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use mipidsi::{Builder, Orientation};
+use std::thread;
+use std::time::Duration;
 
 fn main() -> Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -54,8 +55,8 @@ fn main() -> Result<()> {
     // create driver
     let mut display = Builder::st7789(di)
         .with_display_size(320, 170)
-        .with_orientation(Orientation::Portrait(false))
-        .with_color_order(mipidsi::ColorOrder::Bgr)
+        .with_orientation(Orientation::Landscape(true))
+        .with_invert_colors(mipidsi::ColorInversion::Inverted)
         .init(&mut delay, Some(rst))
         .unwrap();
 
@@ -65,17 +66,38 @@ fn main() -> Result<()> {
     //let ferris = Image::new(&raw_image_data, Point::new(0, 0));
 
     // draw image on red background
-    display.clear(Rgb565::GREEN).unwrap();
     //ferris.draw(&mut display).unwrap();
+    display.clear(Rgb565::BLACK).unwrap();
 
     println!("Cleared display!");
 
     loop {
-        //display.clear(Rgb565::RED).unwrap();
-        thread::sleep(Duration::from_millis(1000));
-        //display.clear(Rgb565::YELLOW).unwrap();
-        //thread::sleep(Duration::from_millis(1000));
-        //display.clear(Rgb565::BLUE).unwrap();
-        //thread::sleep(Duration::from_millis(1000));
+        set_display_color(&mut display, Rgb565::GREEN, "GREEN")?;
+        set_display_color(&mut display, Rgb565::RED, "RED")?;
+        set_display_color(&mut display, Rgb565::YELLOW, "YELLOW")?;
+        set_display_color(&mut display, Rgb565::BLUE, "BLUE")?;
     }
+}
+
+fn set_display_color<DT>(display: &mut DT, color: Rgb565, text: &str) -> Result<()>
+where
+    DT: DrawTarget<Color = Rgb565>,
+{
+    println!("Draw color {text}");
+
+    Rectangle::new(Point::new(0, 0), Size::new(200, 100))
+        .into_styled(PrimitiveStyleBuilder::new().fill_color(color).build())
+        .draw(display)
+        .map_err(|_| anyhow!("unable to draw rectangle"))?;
+
+    // Create a new character style
+    let style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+
+    Text::new(text, Point::new(100, 50), style)
+        .draw(display)
+        .map_err(|_| anyhow!("unable to draw text"))?;
+
+    thread::sleep(Duration::from_secs(10));
+
+    Ok(())
 }
